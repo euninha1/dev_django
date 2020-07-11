e449eb3d725a82ca783e4a8d1ef6c6e6d88fc6c1
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-07-11 12:02:48 -0300

    Editar/Excluir

--- core/views.py
+++ core/views.py
@@ -6,8 +6,8 @@ from django.contrib import messages
 
 # Create your views here.
 
-'''def index(request):
-    return redirect('/agenda/')'''
+#def index(request):
+    #return redirect('/agenda/')
 
 def login_user(request):
     return render(request, 'login.html')
@@ -39,7 +39,11 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 
 @login_required(login_url='/login/')
@@ -49,9 +53,34 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objetcs.create(titulo=titulo,
-                              data_evento=data_evento,
-                              descricao=descricao,
-                              usuario=usuario)
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.save()
+            #Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                          data_evento=data_evento,
+            #                                           descricao=descricao)
+        else:
+            Evento.objetcs.create(titulo=titulo,
+                                  data_evento=data_evento,
+                                  descricao=descricao,
+                                  usuario=usuario)
 
     return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect ('/')
+
+
+
+
+
